8832cb93ad4f1d4f09eec096fcd38f20f9ab1d33
M.Kozak <kozak_m@outlook.com>
2018-11-13 21:03:41 +0100

    Updates

---
+++ precache-manifest.5fd4cf4d22c66ab9380ffa3b528f94c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Hair-Salon/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df464882a5d3c2237da2",
+    "url": "/React-Hair-Salon/static/js/runtime~main.df464882.js"
+  },
+  {
+    "revision": "4ad5f25a7fd32ad63aa8",
+    "url": "/React-Hair-Salon/static/js/main.4ad5f25a.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/React-Hair-Salon/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "4ad5f25a7fd32ad63aa8",
+    "url": "/React-Hair-Salon/static/css/main.0963dc68.chunk.css"
+  },
+  {
+    "revision": "211eae613ff59f438fdf904e1be61f54",
+    "url": "/React-Hair-Salon/index.html"
+  }
+];
