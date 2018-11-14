dc8a3c58e181ed56f4deb98d688e8ab40b6c71ab
M.Kozak <kozak_m@outlook.com>
2018-11-14 20:39:01 +0100

    createProject

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -39,7 +39,7 @@ class SignUp extends Component{
                     </div>
                     <div className="input-field">
                         <button className="btn pink lighten-1 z-depth-0">
-                            Login
+                            Sign Up
                         </button>
                     </div>
                 </form>
