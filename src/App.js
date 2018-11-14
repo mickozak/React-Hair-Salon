0b753429fe1307c9e22963bf4c252272866e9406
M.Kozak <kozak_m@outlook.com>
2018-11-14 20:29:39 +0100

    SignIn&SignUp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
-import Dashboard from "./components/dashboard/Dashboard";
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 class App extends Component {
   render() {
@@ -10,7 +13,10 @@ class App extends Component {
           <div className="App">
             <Navbar/>
             <Switch>
-                <Route path='/' component={Dashboard}/>
+                <Route exact path='/' component={Dashboard}/>
+                <Route path='/project/:id' component={ProjectDetails}/>
+                <Route path='/signin' component={SignIn}/>
+                <Route path='/signup' component={SignUp}/>
             </Switch>
           </div>
         </BrowserRouter>
---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProjectDetails = (props) => {
+    const id = props.match.params.id
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-totle">Project Title {id}</span>
+                    <p>Lorem ipsum...</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by</div>
+                    <div>2nd August, 3pm</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
