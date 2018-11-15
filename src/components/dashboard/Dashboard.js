18eeccbf1ce8af0608753d643d3e92ea8cdbc96f
M.Kozak <kozak_m@outlook.com>
2018-11-15 20:16:57 +0100

    addingData

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
+import { connect } from 'react-redux'
 
 class Dashboard extends Component{
     render(){
+        const {projects} = this.props
         return(
            <div className="dashboard container">
                <div className="row">
                    <div className="col s12 m6">
-                       <ProjectList/>
+                       <ProjectList projects={projects}/>
                    </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications/>
@@ -17,7 +19,14 @@ class Dashboard extends Component{
            </div>
         )
     }
-
 }
 
-export default Dashboard
+const mapStateToProps = (state) => {
+    return {
+        projects: state.project.projects
+    }
+}
+
+// mapStateToProps --> project z rootReducers a projects z projectReducer
+
+export default connect (mapStateToProps)(Dashboard)
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+    projects: [
+        {id: 1, title: 'One', content: 'Lorem ipsum one...'},
+        {id: 2, title: 'Two', content: 'Lorem ipsum two...'},
+        {id: 3, title: 'Three', content: 'Lorem ipsum three...'}
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
