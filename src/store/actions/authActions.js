91df39398b8f05e593f1c75d02323c90a6593f80
M.Kozak <kozak_m@outlook.com>
2018-11-22 20:31:38 +0100

    SignIn

---
+++ src/store/actions/authActions.js
@@ -0,0 +1,16 @@
+export const signIn = (credentials) => {
+    return (dispatch, getstate, {getFirebase}) => {
+        const firebase = getFirebase()
+
+        firebase.auth().signInWithEmailAndPassword(
+            credentials.email,
+            credentials.password
+        ).then(()=>{
+            dispatch({type: 'LOGIN_SUCCESS'})
+        }).catch((err)=>{
+            dispatch({type: 'LOGIN_ERROR',err})
+        })
+    }
+}
+
+//musimy przekazac email i pass
