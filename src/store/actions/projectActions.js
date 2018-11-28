6b5b595433fae8b0e3fa7464f159ccf7cf27860d
M.Kozak <kozak_m@outlook.com>
2018-11-28 20:45:18 +0100

    addingProject

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,11 +1,13 @@
 export const createProject = (project) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => {
         const firestore = getFirestore()
+        const profile = getState().firebase.profile
+        const authorId = getState().firebase.auth.uid
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: 'Mic',
-            authorLastName: 'Koz',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
             createAt: new Date()
         }).then(()=>{
             dispatch({type: 'CREATE_PROJECT', project})
