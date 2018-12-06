13f3bc39c54dff775810935a7ff5da1e908fef8e
M.Kozak <kozak_m@outlook.com>
2018-12-06 18:03:19 +0100

    showingNotifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,7 +8,7 @@ import {Redirect} from 'react-router-dom'
 
 class Dashboard extends Component{
     render(){
-        const {projects,auth} = this.props
+        const {projects, auth, notifications} = this.props
         if (!auth.uid) return <Redirect to='/signin'/>
         return(
            <div className="dashboard container">
@@ -17,7 +17,7 @@ class Dashboard extends Component{
                        <ProjectList projects={projects}/>
                    </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications/>
+                        <Notifications notifications={notifications}/>
                     </div>
                </div>
            </div>
@@ -29,7 +29,8 @@ const mapStateToProps = (state) => {
     console.log(state)
     return {
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 
@@ -38,7 +39,8 @@ const mapStateToProps = (state) => {
 export default compose(
     connect (mapStateToProps),
     firestoreConnect([
-        {collection: 'projects'}
+        {collection: 'projects'},
+        {collection: 'notifications', limit: 3}
     ])
 )(Dashboard)
 
